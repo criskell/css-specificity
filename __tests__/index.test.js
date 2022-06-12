fbed6b0b1579abc7fe8b868bdcb5d1aaf097becc
Cristiano <cris.moraes.ds@gmail.com>
2022-06-12 18:31:00 -0300

    Ignorar o seletor universal

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,7 +4,7 @@ const { specificity } = require("../lib");
 test("specificity: should calculate selector specificity", (t) => {
   t.deepEqual(
     specificity(
-      "[x=','], b, #example > [id=example] + a ~ .foo[class=x][data-k][id=x]:hover div#a#b#c span div.hello.bar:active:visited [data-x][data-y]::first-line, #a#b#c:not(.p .k, span#a#b.a, #hello)#d#e"
+      "[x=','], b, #example > [id=example] + a ~ .foo[class=x][data-k][id=x]:hover div#a#b#c span div.hello.bar:active:visited [data-x][data-y]::first-line, #a#b#c:not(.p .k, span#a#b.a, #hello)#d#e > *"
     ),
     [
       [0, 1, 0],
